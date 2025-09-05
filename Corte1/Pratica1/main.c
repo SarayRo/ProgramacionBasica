a0876ec11b37cdfcad31e843f58cc5e3f5a5a967
SarayRo <sarayrodriguezcifuentes88@gmail.com>
2025-09-05 13:10:06 -0500

    Add files via upload

---
+++ Corte1/Pratica1/main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* 
+run this program using the console pauser or add your own getch, system("pause") or input loop 
+Hola estoy precticando programacion
+
+*/
+
+//Es que quiero aprender a programar en C
+
+int main(int argc, char *argv[]) {
+	
+	printf("Hola Mundo");
+	return 0;
+}
