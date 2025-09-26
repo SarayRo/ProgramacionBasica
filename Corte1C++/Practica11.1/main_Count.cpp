879845e34d4f78ae8d07a5ef1637a0afcaa77284
SarayRo <sarayrodriguezcifuentes88@gmail.com>
2025-09-25 22:27:23 -0500

    Add files via upload

---
+++ Corte1C++/Practica11.1/main_Count.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <unistd.h>
+#include <windows.h>
+
+using namespace std;
+
+int main() 
+{ for (int count = 0; count <= 10; count++){
+	
+	cout << count << endl;
+	
+	sleep(1);
+}
+	return 0;
+}
