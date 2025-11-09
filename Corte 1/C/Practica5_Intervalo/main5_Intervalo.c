05c714ca4461e747711ef0ff3ca602c90599546d
SarayRo <sarayrodriguezcifuentes88@gmail.com>
2025-11-09 17:17:23 -0500

    Add files via upload

---
+++ Corte 1/C/Practica5_Intervalo/main5_Intervalo.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(void) {
+	int i;
+	 for (i = 12; i < 20 ; i++) {
+      printf("i = %d\n", i);
+	}
+	return 0;
+}
